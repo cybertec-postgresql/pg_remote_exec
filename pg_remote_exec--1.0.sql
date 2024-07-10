9271dbe66138e0031e5e2834d04cc2bc9f82638e
Ivan Kovmir <ivan.kovmir@cybertec.at>
2024-07-10 14:55:23 +0200

    Only the superuser can call those functions
    Otherwise this is a security risk.

--- pg_remote_exec--1.0.sql
+++ pg_remote_exec--1.0.sql
@@ -10,3 +10,6 @@ CREATE FUNCTION pg_remote_exec_fetch(text, bool)
 RETURNS SETOF text
 AS 'MODULE_PATHNAME', 'pg_remote_exec_fetch'
 LANGUAGE C VOLATILE STRICT;
+
+REVOKE ALL ON FUNCTION pg_remote_exec() FROM PUBLIC;
+REVOKE ALL ON FUNCTION pg_remote_exec_fetch() FROM PUBLIC;
